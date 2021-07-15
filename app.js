75b4eb7b6ee1e4f186e2a48b61559f792c05428c
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-07-15 11:52:31 +0530

    Project setup

---
+++ app.js
@@ -0,0 +1,16 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view-engine','ejs');
+app.use(bodyParser.urlencoded({urlencoded:true}));
+
+
+app.listen(3000,function(){
+    console.log("Server started on port 3000");
+});
+
