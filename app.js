c1811a6ba331d2eb55be0d86c08e33bcadee0d55
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-07-17 12:47:39 +0530

    set env vars

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     password : String
 });
 const secret = "Thisisourlittlesecret";
-userSchema.plugin(encrypt,{secret:secret, encryptedFields:["password"]});
+userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
 const User = mongoose.model("User",userSchema);
 
 app.get("/",function(req,res){
