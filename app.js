dd5565babb7ee61ede60fc17a652e26ea2ece1d2
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-07-22 12:39:38 +0530

    Implementing sign in with google

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ mongoose.set("useCreateIndex",true);
 
 const userSchema = new mongoose.Schema({
     email:String,
-    password : String
+    password : String,
+    googleId : string
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -41,12 +42,20 @@ userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User",userSchema);
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user,done){
+    done(null,user.id);
+});
+passport.deserializeUser(function(id,done){
+    User.findById(id,function(err,user){
+        done(err,user);
+    });
+});
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
+passport.use(
+    new GoogleStrategy(
+        {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/secrets",
     userProfileURL: "https://googleapis.com/oauth2/v3/userinfo"
   },
@@ -64,6 +73,15 @@ app.get("/",function(req,res){
 app.get("/auth/google", function(req,res){
     passport.authenticate("google", {scope:["profile"]});
 });
+
+app.get("/auth/google/secrets", 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/');
+  });
+
+
 app.get("/login",function(req,res){
     res.render("login");
 });
